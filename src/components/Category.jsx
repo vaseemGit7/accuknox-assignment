98bb189790a55f8cbdb93cba8cdf11f3b40c31a4
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-20 01:20:43 +0530

    Update category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,5 +1,29 @@
-const Category = () => {
-  return <></>;
+import { useSelector } from "react-redux";
+
+const Category = ({ categoryId }) => {
+  const data = useSelector((state) => state.dataReducer);
+  const category = data.categories.find(
+    (category) => category.id === categoryId
+  );
+
+  return (
+    <div className="px-3 py-2 border-2 border-neutral-500">
+      <p className="text-base font-semibold text-neutral-900">
+        {category.name}
+      </p>
+      <div className="flex gap-1 overflow-x-scroll">
+        {category.widgets.map((widget) => (
+          <div
+            className="p-2 flex flex-col rounded-md border-2 border-neutral-800"
+            key={widget.id}
+          >
+            <p>{widget.widgetName}</p>
+            <p>{widget.widgetText}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
