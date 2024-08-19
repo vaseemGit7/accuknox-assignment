a7c891e4e86c98f4da9bd78151ed02568fa8520b
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-19 23:06:14 +0530

    Update Dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux";
+
 const Dashboard = () => {
-  return <></>;
+  const data = useSelector((state) => state.dataReducer);
+  return (
+    <>
+      {data.categories.map((category) => (
+        <div
+          className="flex flex-row border-2 border-red-500"
+          key={category.id}
+        >
+          <p>{category.name}</p>
+          <p>{category.widgets}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Dashboard;
