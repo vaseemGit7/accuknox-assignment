4b94f468bc7e3455bbde663628ea5d7a264801c4
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-20 03:06:15 +0530

    Update data reducer

--- src/reducers/dataReducer.jsx
+++ src/reducers/dataReducer.jsx
@@ -1,20 +1,43 @@
+export const ADD_WIDGET = "ADD_WIDGET";
+
 const initialState = {
   categories: [
     {
       id: "1",
       name: "Category 1",
-      widgets: ["testOne"],
+      widgets: [
+        { id: "11", widgetName: "testOne", widgetText: "testOne is a test" },
+        {
+          id: "12",
+          widgetName: "testThree",
+          widgetText: "testThree is a test",
+        },
+      ],
     },
     {
       id: "2",
       name: "Category 2",
-      widgets: ["testTwo"],
+      widgets: [
+        { id: "22", widgetName: "testTwo", widgetText: "testTwo is a test" },
+      ],
     },
   ],
 };
 
-const dataReducer = (state = initialState, actions) => {
-  switch (actions.type) {
+const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_WIDGET:
+      return {
+        ...state,
+        categories: state.categories.map((category) =>
+          category.id === action.payload.categoryId
+            ? {
+                ...category,
+                widgets: [...category.widgets, action.payload.widget],
+              }
+            : category
+        ),
+      };
     default:
       return state;
   }
