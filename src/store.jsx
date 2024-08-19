c0e87fd3ad50c88afeaf2a55049f346a120c3402
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-19 23:04:57 +0530

    Create redux store

---
+++ src/store.jsx
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from "redux";
+import dataReducer from "./reducers/dataReducer";
+
+const combinedReducer = combineReducers({
+  dataReducer: dataReducer,
+});
+
+const store = createStore(combinedReducer);
+
+export default store;
