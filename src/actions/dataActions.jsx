f06c64ae4e6bb7bb3e435501a8682ad31d47fea1
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-20 20:19:10 +0530

    Update actions to remove widget

--- src/actions/dataActions.jsx
+++ src/actions/dataActions.jsx
@@ -1,6 +1,12 @@
 import { ADD_WIDGET } from "../reducers/dataReducer";
+import { REMOVE_WIDGET } from "../reducers/dataReducer";
 
 export const addWidget = (categoryId, widget) => ({
   type: ADD_WIDGET,
   payload: { categoryId, widget },
+});
+
+export const removeWidget = (categoryId, widgetId) => ({
+  type: REMOVE_WIDGET,
+  payload: { categoryId, widgetId },
 });
