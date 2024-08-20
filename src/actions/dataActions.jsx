3313af92562471887fa3ba70dad2edde2d6502b9
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-20 20:45:29 +0530

    Update actions to toggle widget

--- src/actions/dataActions.jsx
+++ src/actions/dataActions.jsx
@@ -1,5 +1,6 @@
 import { ADD_WIDGET } from "../reducers/dataReducer";
 import { REMOVE_WIDGET } from "../reducers/dataReducer";
+import { TOGGLE_WIDGET } from "../reducers/dataReducer";
 
 export const addWidget = (categoryId, widget) => ({
   type: ADD_WIDGET,
@@ -8,5 +9,10 @@ export const addWidget = (categoryId, widget) => ({
 
 export const removeWidget = (categoryId, widgetId) => ({
   type: REMOVE_WIDGET,
+  payload: { categoryId, widgetId },
+});
+
+export const toggleWidget = (categoryId, widgetId) => ({
+  type: TOGGLE_WIDGET,
   payload: { categoryId, widgetId },
 });
