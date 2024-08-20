1655786ed444742e8388bed2723d5bfa3d73e49d
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-20 20:44:56 +0530

    Update reducer to toggle widget

--- src/reducers/dataReducer.jsx
+++ src/reducers/dataReducer.jsx
@@ -1,5 +1,6 @@
 export const ADD_WIDGET = "ADD_WIDGET";
 export const REMOVE_WIDGET = "REMOVE_WIDGET";
+export const TOGGLE_WIDGET = "TOGGLE_WIDGET";
 
 const initialState = {
   categories: [
@@ -7,11 +8,17 @@ const initialState = {
       id: "1",
       name: "Category 1",
       widgets: [
-        { id: "11", widgetName: "testOne", widgetText: "testOne is a test" },
+        {
+          id: "11",
+          widgetName: "testOne",
+          widgetText: "testOne is a test",
+          widgetChecked: true,
+        },
         {
           id: "12",
           widgetName: "testThree",
           widgetText: "testThree is a test",
+          widgetChecked: true,
         },
       ],
     },
@@ -19,7 +26,12 @@ const initialState = {
       id: "2",
       name: "Category 2",
       widgets: [
-        { id: "22", widgetName: "testTwo", widgetText: "testTwo is a test" },
+        {
+          id: "22",
+          widgetName: "testTwo",
+          widgetText: "testTwo is a test",
+          widgetChecked: true,
+        },
       ],
     },
   ],
@@ -53,6 +65,23 @@ const dataReducer = (state = initialState, action) => {
             : category
         ),
       };
+    case TOGGLE_WIDGET:
+      return {
+        ...state,
+        categories: state.categories.map((category) =>
+          category.id === action.payload.categoryId
+            ? {
+                ...category,
+                widgets: category.widgets.map((widget) =>
+                  widget.id === action.payload.widgetId
+                    ? { ...widget, widgetChecked: !widget.widgetChecked }
+                    : widget
+                ),
+              }
+            : category
+        ),
+      };
+
     default:
       return state;
   }
