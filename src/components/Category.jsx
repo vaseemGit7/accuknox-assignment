5ae4084811b2ecfd0dcc4b6699ef7290b512b0db
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-21 15:28:55 +0530

    Update Category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,16 @@
 import { useSelector } from "react-redux";
 import AddModal from "./AddModal";
 import { useState } from "react";
+import { IonIcon } from "@ionic/react";
+import { useDispatch } from "react-redux";
+import { addOutline, closeOutline } from "ionicons/icons";
+import { toggleWidget } from "../actions/dataActions";
 
 const Category = ({ categoryId }) => {
   const data = useSelector((state) => state.dataReducer);
   const [dialogToggle, setDialogToggle] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(null);
+  const dispatch = useDispatch();
 
   const category = data.categories.find(
     (category) => category.id === categoryId
@@ -19,37 +24,64 @@ const Category = ({ categoryId }) => {
     setDialogToggle((prevState) => !prevState);
   };
 
+  const handleToggleWidget = (widgetId) => {
+    dispatch(toggleWidget(categoryId, widgetId));
+  };
+
   console.log(currentCategory);
 
   return (
-    <div className="px-3 py-2 border-2 border-neutral-500">
-      <p className="text-base font-semibold text-neutral-900">
-        {category.name}
-      </p>
-      <div className="flex gap-1 overflow-x-scroll">
+    <div className="px-3 py-2">
+      <p className="text-lg font-semibold text-neutral-900">{category.name}</p>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(550px, 1fr))",
+          gap: "15px",
+        }}
+        className="p-1 overflow-x-scroll"
+      >
         <>
           {category.widgets.map(
             (widget) =>
               widget.widgetChecked && (
                 <div
-                  className="p-2 flex flex-col rounded-md border-2 border-neutral-800"
+                  className="p-4 h-56 flex flex-row justify-between rounded-2xl outline outline-1 outline-neutral-200 bg-[#ffffff] drop-shadow-sm relative group hover:scale-[1.015] "
                   key={widget.id}
                 >
-                  <p>{widget.widgetName}</p>
-                  <p>{widget.widgetText}</p>
+                  <div className="flex flex-col gap-3">
+                    <p className="text-base font-semibold text-neutral-800">
+                      {widget.widgetName}
+                    </p>
+                    <p className="text-sm font-medium text-neutral-800">
+                      {widget.widgetText}
+                    </p>
+                  </div>
+                  <div className="self-start">
+                    <button
+                      className="opacity-0 group-hover:opacity-100 border-none "
+                      onClick={() => handleToggleWidget(widget.id)}
+                    >
+                      <IonIcon
+                        icon={closeOutline}
+                        className=" text-neutral-600 text-xl cursor-pointer hover:scale-[1.05]"
+                      />
+                    </button>
+                  </div>
                 </div>
               )
           )}
         </>
-        <div className="p-2 flex items-center rounded-md border-2 border-neutral-800">
+        <div className="p-2 flex min-h-56 justify-center outline outline-1 outline-neutral-200 items-center rounded-2xl bg-[#ffffff] drop-shadow-sm">
           <button
-            className="border-2 border-neutral-800 rounded p-1"
+            className="flex gap-2 items-center outline outline-1 outline-neutral-400 text-neutral-500 font-medium rounded py-1 px-3 hover:bg-neutral-100"
             onClick={() => {
               handleCurrentCategory(category);
               handleDialogToggle();
             }}
           >
-            + Add Widget
+            <IonIcon icon={addOutline} className="text-lg" />
+            Add Widget
           </button>
         </div>
       </div>
