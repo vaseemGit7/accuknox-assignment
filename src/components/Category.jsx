048dd3a3b3a53b438d462ef924d1c0b1105101cd
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-08-21 23:34:05 +0530

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,10 +3,11 @@ import AddModal from "./AddModal";
 import { useState } from "react";
 import { IonIcon } from "@ionic/react";
 import { useDispatch } from "react-redux";
-import { addOutline, closeOutline } from "ionicons/icons";
+import { addOutline } from "ionicons/icons";
 import { toggleWidget } from "../actions/dataActions";
+import Widget from "./Widget";
 
-const Category = ({ categoryId }) => {
+const Category = ({ categoryId, queryWidgets }) => {
   const data = useSelector((state) => state.dataReducer);
   const [dialogToggle, setDialogToggle] = useState(false);
   const [currentCategoryId, setCurrentCategoryId] = useState(null);
@@ -29,7 +30,7 @@ const Category = ({ categoryId }) => {
   };
 
   return (
-    <div className="px-3 py-2">
+    <div className="px-3 py-1">
       <p className="text-lg font-semibold text-neutral-900">{category.name}</p>
       <div
         style={{
@@ -40,48 +41,25 @@ const Category = ({ categoryId }) => {
         className="p-1 overflow-x-scroll"
       >
         <>
-          {category.widgets.map(
-            (widget) =>
-              widget.widgetChecked && (
-                <div
-                  className="p-4 h-56 flex flex-row justify-between rounded-2xl outline outline-1 outline-neutral-200 bg-[#ffffff] drop-shadow-sm relative group hover:scale-[1.015] "
-                  key={widget.id}
-                >
-                  <div className="flex flex-col gap-3">
-                    <p className="text-base font-semibold text-neutral-800">
-                      {widget.widgetName}
-                    </p>
-                    <p className="text-sm font-medium text-neutral-800">
-                      {widget.widgetText}
-                    </p>
-                  </div>
-                  <div className="self-start">
-                    <button
-                      className="opacity-0 group-hover:opacity-100 border-none "
-                      onClick={() => handleToggleWidget(widget.id)}
-                    >
-                      <IonIcon
-                        icon={closeOutline}
-                        className=" text-neutral-600 text-xl cursor-pointer hover:scale-[1.05]"
-                      />
-                    </button>
-                  </div>
-                </div>
-              )
-          )}
+          <Widget
+            widgets={queryWidgets ? queryWidgets : category.widgets}
+            handleToggleWidget={handleToggleWidget}
+          />
         </>
-        <div className="p-2 flex min-h-56 justify-center outline outline-1 outline-neutral-200 items-center rounded-2xl bg-[#ffffff] drop-shadow-sm">
-          <button
-            className="flex gap-2 items-center outline outline-1 outline-neutral-400 text-neutral-500 font-medium rounded py-1 px-3 hover:bg-neutral-100"
-            onClick={() => {
-              handleCurrentCategoryId(category.id);
-              handleDialogToggle();
-            }}
-          >
-            <IonIcon icon={addOutline} className="text-lg" />
-            Add Widget
-          </button>
-        </div>
+        {!queryWidgets && (
+          <div className="p-2 flex min-h-56 justify-center outline outline-1 outline-neutral-200 items-center rounded-2xl bg-[#ffffff] drop-shadow-sm">
+            <button
+              className="flex gap-2 items-center outline outline-1 outline-neutral-400 text-neutral-500 font-medium rounded py-1 px-3 hover:bg-neutral-100"
+              onClick={() => {
+                handleCurrentCategoryId(category.id);
+                handleDialogToggle();
+              }}
+            >
+              <IonIcon icon={addOutline} className="text-lg" />
+              Add Widget
+            </button>
+          </div>
+        )}
       </div>
       <AddModal
         data={data}
